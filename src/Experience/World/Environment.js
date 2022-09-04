438cbb099baf3d921c9d7fb97ff9d6b1c1e77504
Niko <63638951+NikoGham@users.noreply.github.com>
2022-09-04 17:27:46 +0100

    edit environment

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -14,65 +14,66 @@ export default class Environment {
 		}
 
 		// this.setSunLight()
-		this.setAmbientLight()
+		// this.setAmbientLight()
 		this.setEnvironmentMap()
 	}
 
-	setAmbientLight() {
-		this.ambient = new THREE.AmbientLight('#ffffff', 5)
-		if (this.debug.active) {
-			this.debugFolder
-				.add(this.ambient, 'intensity')
-				.name('ambient')
-				.min(0)
-				.max(50)
-				.step(0.001)
-		}
-	}
-	setSunLight() {
-		this.sunLight = new THREE.DirectionalLight('#ffffff', 4)
-		this.sunLight.castShadow = true
-		this.sunLight.shadow.camera.far = 15
-		this.sunLight.shadow.mapSize.set(1024, 1024)
-		this.sunLight.shadow.normalBias = 0.05
-		this.sunLight.position.set(3.5, 2, -1.25)
-		this.scene.add(this.sunLight)
+	// setAmbientLight() {
+	// 	this.ambient = new THREE.AmbientLight('#ffffff', 5)
+	// 	if (this.debug.active) {
+	// 		this.debugFolder
+	// 			.add(this.ambient, 'intensity')
+	// 			.name('ambient')
+	// 			.min(0)
+	// 			.max(50)
+	// 			.step(0.001)
+	// 	}
+	// }
 
-		// Debug
-		if (this.debug.active) {
-			this.debugFolder
-				.add(this.sunLight, 'intensity')
-				.name('sunLightIntensity')
-				.min(0)
-				.max(10)
-				.step(0.001)
+	// setSunLight() {
+	// 	this.sunLight = new THREE.DirectionalLight('#ffffff', 4)
+	// 	this.sunLight.castShadow = true
+	// 	this.sunLight.shadow.camera.far = 15
+	// 	this.sunLight.shadow.mapSize.set(1024, 1024)
+	// 	this.sunLight.shadow.normalBias = 0.05
+	// 	this.sunLight.position.set(3.5, 2, -1.25)
+	// 	this.scene.add(this.sunLight)
 
-			this.debugFolder
-				.add(this.sunLight.position, 'x')
-				.name('sunLightX')
-				.min(-5)
-				.max(5)
-				.step(0.001)
+	// 	// Debug
+	// 	if (this.debug.active) {
+	// 		this.debugFolder
+	// 			.add(this.sunLight, 'intensity')
+	// 			.name('sunLightIntensity')
+	// 			.min(0)
+	// 			.max(10)
+	// 			.step(0.001)
 
-			this.debugFolder
-				.add(this.sunLight.position, 'y')
-				.name('sunLightY')
-				.min(-5)
-				.max(5)
-				.step(0.001)
+	// 		this.debugFolder
+	// 			.add(this.sunLight.position, 'x')
+	// 			.name('sunLightX')
+	// 			.min(-5)
+	// 			.max(5)
+	// 			.step(0.001)
 
-			this.debugFolder
-				.add(this.sunLight.position, 'z')
-				.name('sunLightZ')
-				.min(-5)
-				.max(5)
-				.step(0.001)
-		}
-	}
+	// 		this.debugFolder
+	// 			.add(this.sunLight.position, 'y')
+	// 			.name('sunLightY')
+	// 			.min(-5)
+	// 			.max(5)
+	// 			.step(0.001)
+
+	// 		this.debugFolder
+	// 			.add(this.sunLight.position, 'z')
+	// 			.name('sunLightZ')
+	// 			.min(-5)
+	// 			.max(5)
+	// 			.step(0.001)
+	// 	}
+	// }
 
 	setEnvironmentMap() {
 		this.environmentMap = {}
-		this.environmentMap.intensity = 0.4
+		this.environmentMap.intensity = 3
 		this.environmentMap.texture = this.resources.items.environmentMapTexture
 		this.environmentMap.texture.encoding = THREE.sRGBEncoding
 
